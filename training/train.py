6cc0fb7907f10a79a8415283b4ca9b8be4c06626
SatyabrataBhattacharjee <satyabratbhattacharya1408@gmail.com>
2026-01-03 15:53:42 +0530

    Create train.py

---
+++ training/train.py
@@ -0,0 +1,27 @@
+import torch
+from training.losses import calc_loss_batch
+from training.eval import calc_loss_loader
+from generation.sample import generate_and_print_sample
+
+def train_model_simple(
+    model, train_loader, val_loader, optimizer, device,
+    num_epochs, eval_freq, eval_iter, start_context, tokenizer
+):
+    step = 0
+
+    for epoch in range(num_epochs):
+        model.train()
+        for x, y in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(x, y, model, device)
+            loss.backward()
+            optimizer.step()
+
+            if step % eval_freq == 0:
+                train_loss = calc_loss_loader(train_loader, model, device, eval_iter)
+                val_loss = calc_loss_loader(val_loader, model, device, eval_iter)
+                print(f"Epoch {epoch+1} Step {step}: Train {train_loss:.3f}, Val {val_loss:.3f}")
+
+            step += 1
+
+        generate_and_print_sample(model, tokenizer, device, start_context)
